818cc700b243b3865b929a35ff2b5b1229444c2c
Alekss Dzonsons <A240180AD@rvt.lv>
2025-09-23 14:01:24 +0300

    Gift tax

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,12 +1,33 @@
 package rvt;
 import java.util.Scanner;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
-
 public class App {
     public static void main(String[] args) {
-        password();
+        giftTax();
         
+    }
+
+    public static void giftTax(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gift value?");
+        int giftValue = Integer.valueOf(scanner.nextLine());
+        double tax;
+
+        if(giftValue < 5000){
+            tax = 0;
+        } else if (giftValue < 25000) {
+            tax = 100 + (giftValue - 5000) * 0.08;
+        } else if (giftValue < 55000) {
+            tax = 1700 + (giftValue - 25000) * 0.1;
+        } else if (giftValue < 200000){
+            tax = 4700 + (giftValue - 55000) * 0.12;
+        } else if (giftValue < 1000000) {
+            tax = 22100 + (giftValue - 200000) * 0.15;
+        } else {
+            tax = 142100 + (giftValue - 1000000) * 0.17;
+        }
+
+        System.out.println("Tax: " + tax);
     }
 
     public static void password(){
