d8f3d5a2a474cc4251fe0017203246c1afe54fac
Alekss Dzonsons <A240180AD@rvt.lv>
2025-10-09 12:45:03 +0300

    Two Array

--- src/main/java/rvt/Connecticut.java
+++ src/main/java/rvt/Connecticut.java
@@ -2,8 +2,7 @@ package rvt;
 
 public class Connecticut{
     public static void main(String[] args){
-        int[] numbers = {0, 1, 2, 3};
-        System.out.println(arraySum(numbers));
+        twoArray();
     }
 
     public static int arraySum(int[] array){
@@ -12,5 +11,22 @@ public class Connecticut{
             sum += array[i];
         }
         return sum;
+    }
+
+    public static void twoArray(){
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice = new int[val.length];
+        
+        System.out.println( "Original Array: " 
+           + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    
+        for(int i = 0; i < val.length; i++){
+            twice[i] = val[i] * 2;
+        }
+
+    
+        System.out.println( "New Array: " 
+            + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+
     }
 }
